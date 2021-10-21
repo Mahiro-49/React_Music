59f69cd6fa126e1f97cae1e8aee3d12ab3b9a735
Mash1ro <9376303+mingjian49@user.noreply.gitee.com>
2021-10-21 23:52:00 +0800

    '排行榜基本完成'

--- src/pages/discover/c-pages/ranking/c-cpns/list/index.js
+++ src/pages/discover/c-pages/ranking/c-cpns/list/index.js
@@ -2,10 +2,71 @@ import React, { memo } from 'react'
 
 import { useSelector, shallowEqual } from 'react-redux'
 
+import { RankingListWrapper } from "./style"
+
+import { getSizeImage, formatMinuteSecond } from "@/utils/format-utils.js"
+
 export default memo(function MJRankList() {
+  const { playList } = useSelector(state => ({
+    playList: state.getIn(["ranking", "playList"])
+  }), shallowEqual)
+
+  const tracks = playList.tracks || []
+
+
+
   return (
-    <div>
-      <h2>MJRankList</h2>
-    </div>
+    <RankingListWrapper>
+      <div className="header">
+        <div className="left">
+          <h3 className="title">歌曲列表</h3>
+          <div className="count-1">{playList.trackCount}首歌</div>
+        </div>
+        <div className="right">
+          <span>播放:</span>
+          <span className="count-2">{playList.playCount}</span>
+          <span>次</span>
+        </div>
+      </div>
+      <div className="play-list">
+        <table>
+          <thead>
+            <tr className="list-header">
+              <th className="rank"></th>
+              <th className="title">标题</th>
+              <th className="duration">时长</th>
+              <th className="singer">歌手</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              tracks.map((item, index) => {
+                return (
+                  <tr key={item.id}>
+                    <td>
+                      <div className="rank-num">
+                        <span className="num">{index + 1}</span>
+                        <span className="new sprite_icon2"></span>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="song-name">
+                        {
+                          index < 3 ? (<img src={getSizeImage(item.al.picUrl, 50)} alt="" />) : null
+                        }
+                        <span className="play sprite_table"></span>
+                        <span className="name">{item.name}</span>
+                      </div>
+                    </td>
+                    <td>{formatMinuteSecond(item.dt)}</td>
+                    <td>{item.ar[0].name}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </RankingListWrapper>
   )
 })
--- src/pages/discover/c-pages/ranking/c-cpns/list/style.js
+++ src/pages/discover/c-pages/ranking/c-cpns/list/style.js
@@ -1,1 +1,113 @@
 import styled from "styled-components";
+
+export const RankingListWrapper = styled.div`
+  padding: 0 40px;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 2px solid #c20c0c;
+
+    .left {
+      display: flex;
+      .title {
+        font-size: 20px;
+      }
+      .count-1 {
+        margin: auto 0;
+        margin-left: 15px;
+      }
+    }
+    .right {
+      .count-2 {
+        color: #c20c0c;
+        font-weight: 700;
+        margin-left: 5px;
+      }
+    }
+  }
+
+  .play-list {
+    table {
+      width: 100%;
+      border: 1px solid #d9d9d9;
+      thead {
+        .list-header {
+          th {
+            height: 34px;
+            line-height: 34px;
+            background-image: url(${require("@/assets/img/sprite_table.png").default});
+            color: #666;
+            border: 1px solid #ddd;
+            border-width: 0 0 1px 1px;
+            font-weight: normal;
+            text-align: left;
+            padding-left: 10px;
+          }
+          .rank {
+            width: 78px;
+            border-left: none;
+          }
+          .duration {
+            width: 91px;
+          }
+          .singer {
+            width: 173px;
+          }
+        }
+      }
+      tbody {
+        td {
+          padding: 6px 10px;
+        }
+        tr:nth-child(2n) {
+          background-color: #fff;
+        }
+        tr:nth-child(2n+1) {
+          background-color: #f7f7f7;
+        }
+
+        .rank-num {
+          display: flex;
+          .num {
+            width: 25px;
+            height: 18px;
+            text-align: center;
+            color: #999;
+          }
+          .new {
+            width: 16px;
+            height: 17px;
+            margin-left: 12px;
+            background-position: -67px -283px;
+          }
+        }
+
+        .song-name {
+          display: flex;
+          align-items: center;
+          img {
+            width: 50px;
+            height: 50px;
+            margin-right: 10px;
+          }
+          .play {
+            width: 17px;
+            height: 17px;
+            background-position: 0 -103px;
+            cursor: pointer;
+            :hover {
+              background-position: 0 -128px;
+            }
+          }
+          .name {
+            margin-left: 10px;
+          }
+        }
+      }
+
+    }
+  }
+
+
+`
